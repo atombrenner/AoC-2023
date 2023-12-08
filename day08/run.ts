2bea2e93bc3eb7e30fe9990d9bec865bb74f3f13
Christian Rodemeyer <atombrenner@gmail.com>
2023-12-08 12:46:15 +0100

    day 08 part 2 - semi brute force

--- day08/run.ts
+++ day08/run.ts
@@ -38,4 +38,58 @@ let currentNodes = Object.keys(nodeMap).filter((id) => id.endsWith('A'))
 //   step += 1
 // }
 
-//console.log('steps needed in part 2', step)
+// some debugging code to analyse the data
+const debug = currentNodes.map(() => ({
+  lastStep: 0,
+  distances: new Set<number>(),
+  nodes: new Set<string>(),
+}))
+
+while (step < 1_000_000) {
+  const instruction = instructions[step % instructions.length] === 'L' ? 'left' : 'right'
+  currentNodes = currentNodes.map((id) => nodeMap[id][instruction])
+  step += 1
+  currentNodes.forEach((node, i) => {
+    if (node.endsWith('Z')) {
+      const info = debug[i]
+      //if (info.lastStep >= 0) {
+      //console.log(`node ${i} delta: ${step - info.lastStep}`)
+      info.distances.add(step - info.lastStep)
+      // }
+      info.nodes.add(node)
+      info.lastStep = step
+    }
+  })
+}
+debug.forEach((info, i) => {
+  const nodes = Array.from(info.nodes.values()).join(',')
+  const distances = Array.from(info.distances.values()).join(',')
+  console.log(`${i}: ${nodes} : ${distances}`)
+})
+
+// it looks like we have a stable cycle pattern for each initial node in currentNodes, starting with 0
+
+// 0: LNZ : 20569
+// 1: HPZ : 18727
+// 2: ZZZ : 14429
+// 3: SGZ : 13201
+// 4: CXZ : 18113
+// 5: CFZ : 22411
+
+// every 20569 steps node[0] hits LNZ, every 18727 node[1] hits HPZ and so on
+
+// when will cycles synchronize and all of them hit nodes ending with z?
+
+const syncpoint = 20569 * 18727 * 14429 * 13201 * 18113 * 22411
+console.log(syncpoint) // answer too high
+
+const cycles = [20569, 18727, 14429, 13201, 18113, 22411]
+
+const stepIsDividableByAllCycles = (step: number) => cycles.every((c) => step % c === 0)
+
+const maxCycle = Math.max(...cycles)
+for (step = maxCycle; !stepIsDividableByAllCycles(step); step += maxCycle) {
+  // console.log(step)
+}
+
+console.log('steps needed in part 2', step)
