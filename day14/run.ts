947853e4282c944885c8553448bdf2209dec0d49
Christian Rodemeyer <atombrenner@gmail.com>
2023-12-14 09:46:10 +0100

    day 14 part 2

--- day14/run.ts
+++ day14/run.ts
@@ -1,6 +1,6 @@
 import { readLines } from '../utils/utils'
 
-const lines = readLines('day14/input.txt')
+const lines = readLines('day14/example.txt')
 const platform = lines.map((line) => line.split(''))
 const height = platform.length
 const width = platform[0].length
@@ -10,11 +10,11 @@ const print = (p: string[][]) => {
   console.log()
 }
 
-print(platform)
-
 const moveRocks = (x: number, startY: number) => {
-  let rockCount = 0
   let y = startY
+
+  let rockCount = 0
+
   while (y < height && platform[y][x] !== '#') {
     if (platform[y][x] === 'O') rockCount++
     y++
@@ -27,13 +27,45 @@ const moveRocks = (x: number, startY: number) => {
 }
 
 // destructive move upwards
-for (let x = 0; x < width; ++x) {
-  let y = 0
-  while (y < height) {
-    y = moveRocks(x, y) + 1
+const moveNorth = () => {
+  for (let x = 0; x < width; ++x) {
+    let y = 0
+    while (y < height) {
+      const startY = y
+      let rockY = y
+      while (y < height && platform[y][x] !== '#') {
+        if (platform[y][x] === 'O') rockY++
+        y++
+      }
+      for (let i = startY; i < y; i++) {
+        platform[i][x] = i < rockY ? 'O' : '.'
+      }
+      y++
+    }
   }
 }
 
+const moveSouth = () => {
+  for (let x = 0; x < width; ++x) {
+    let y = height
+    while (--y > 0) {
+      const startY = y
+      let rockY = y
+      while (y >= 0 && platform[y][x] !== '#') {
+        if (platform[y][x] === 'O') rockY--
+        y--
+      }
+      for (let i = startY; i > y; i--) {
+        platform[i][x] = i > rockY ? 'O' : '.'
+      }
+    }
+  }
+}
+
+print(platform)
+moveNorth()
+print(platform)
+moveSouth()
 print(platform)
 
 let load = 0
