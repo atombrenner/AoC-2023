54d5f9ec2b673dde89b8a05008d40bf19dd4d956
Christian Rodemeyer <atombrenner@gmail.com>
2023-12-15 17:37:56 +0100

    day 15 part 2

--- day15/run.ts
+++ day15/run.ts
@@ -15,4 +15,45 @@ const hash = (step: string) => {
   return current
 }
 
-console.log('sum of hashes', sum(steps.map(hash)))
+// console.log('sum of hashes', sum(steps.map(hash)))
+
+type Lens = {
+  label: string
+  focal: number
+}
+
+const boxes: Lens[][] = Array(256)
+  .fill(0)
+  .map(() => [])
+
+const print = () => {
+  boxes.forEach((box, i) => {
+    if (box.length > 0) {
+      console.log(`Box ${i}: ${box.map((lens) => `[${lens.label} ${lens.focal}]`).join(' ')}`)
+    }
+  })
+}
+
+for (const step of steps) {
+  //console.log(step)
+  const [label, focal] = step.split(/=|-/)
+  const i = hash(label)
+  if (step.endsWith('-')) {
+    boxes[i] = boxes[i].filter((lens) => lens.label !== label)
+  } else {
+    const existingLens = boxes[i].find((lens) => lens.label === label)
+    if (existingLens) {
+      existingLens.focal = Number(focal)
+    } else {
+      boxes[i].push({ label, focal: Number(focal) }) // push means adding behind
+    }
+  }
+  //print()
+}
+print()
+
+const total = sum(
+  boxes.flatMap((box, bi) => box.map((lens, li) => (bi + 1) * (li + 1) * lens.focal)),
+)
+
+console.log('total focal power', total)
