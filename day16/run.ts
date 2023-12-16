2288510a7992e92de1b45eca910172aab114c7f2
Christian Rodemeyer <atombrenner@gmail.com>
2023-12-16 16:43:30 +0100

    day 16 part 2

--- day16/run.ts
+++ day16/run.ts
@@ -10,6 +10,7 @@ import { readFileSync } from 'fs'
 
 const input = readFileSync('day16/input.txt', 'utf-8').split('\n')
 const width = input[0].length
+const height = input.length
 const grid = input.join('')
 
 const maxWidth = 1000
@@ -18,7 +19,6 @@ const down = width
 const left = -1
 const right = 1
 
-console.log(width)
 console.assert(width < maxWidth)
 
 // if we have only an offset, how can we detect leaving the grid?
@@ -31,66 +31,80 @@ const move = (pos: number, dir: number) => {
   return next
 }
 
-const energized = new Set<number>()
-const cycle = new Set<number>()
-
-const beams = [[0, right]]
-for (let i = 0; i < beams.length; ++i) {
-  let [pos, dir] = beams[i]
-  while (pos >= 0) {
-    const posAndDir = pos * maxWidth + dir
-    if (cycle.has(posAndDir)) break
-    cycle.add(posAndDir)
-    energized.add(pos)
-    // console.log(pos, dir)
-    switch (grid[pos]) {
-      case '\\':
-        switch (dir) {
-          case left:
-            dir = up
-            break
-          case right:
-            dir = down
-            break
-          case up:
+const calcEnergizedTiles = (startPos: number, startDir: number): number => {
+  const energized = new Set<number>()
+  const cycle = new Set<number>()
+  const beams = [[startPos, startDir]]
+  for (let i = 0; i < beams.length; ++i) {
+    let [pos, dir] = beams[i]
+    while (pos >= 0) {
+      const posAndDir = pos * maxWidth + dir
+      if (cycle.has(posAndDir)) break
+      cycle.add(posAndDir)
+      energized.add(pos)
+      // console.log(pos, dir)
+      switch (grid[pos]) {
+        case '\\':
+          switch (dir) {
+            case left:
+              dir = up
+              break
+            case right:
+              dir = down
+              break
+            case up:
+              dir = left
+              break
+            case down:
+              dir = right
+              break
+          }
+          break
+        case '/':
+          switch (dir) {
+            case left:
+              dir = down
+              break
+            case right:
+              dir = up
+              break
+            case up:
+              dir = right
+              break
+            case down:
+              dir = left
+              break
+          }
+          break
+        case '-':
+          if (dir === up || dir === down) {
             dir = left
-            break
-          case down:
-            dir = right
-            break
-        }
-        break
-      case '/':
-        switch (dir) {
-          case left:
-            dir = down
-            break
-          case right:
+            beams.push([pos, right])
+          }
+          break
+        case '|':
+          if (dir === left || dir === right) {
             dir = up
-            break
-          case up:
-            dir = right
-            break
-          case down:
-            dir = left
-            break
-        }
-        break
-      case '-':
-        if (dir === up || dir === down) {
-          dir = left
-          beams.push([pos, right])
-        }
-        break
-      case '|':
-        if (dir === left || dir === right) {
-          dir = up
-          beams.push([pos, down])
-        }
-        break
+            beams.push([pos, down])
+          }
+          break
+      }
+      pos = move(pos, dir)
     }
-    pos = move(pos, dir)
   }
+  return energized.size
 }
 
-console.log('energized tiles', energized.size)
+console.log('energized part 1: ', calcEnergizedTiles(0, right))
+
+const energized: number[] = []
+for (let y = 0; y < height; ++y) {
+  energized.push(calcEnergizedTiles(y * width, right))
+  energized.push(calcEnergizedTiles((y + 1) * width - 1, left))
+}
+for (let x = 0; x < width; ++x) {
+  energized.push(calcEnergizedTiles(x, down))
+  energized.push(calcEnergizedTiles((height + 1) * width + x, up))
+}
+
+console.log('max energized part 2: ', Math.max(...energized))
